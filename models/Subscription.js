d7945f2f0e6724165608546759c3532f0d8edf7b
ELWAZEIR <hossamshapan88@gmail.com>
2025-08-23 04:46:04 +0300

    make playerId standar

--- models/Subscription.js
+++ models/Subscription.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
 const SubscriptionSchema = new mongoose.Schema({
-  player: { type: mongoose.Schema.Types.ObjectId, ref: 'Player' },
+  // player: { type: mongoose.Schema.Types.ObjectId, ref: 'Player' },
+  playerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Player' },
   month: String,
   hasPaid: Boolean,
   paymentDate: Date,
   amount: Number
 }, { timestamps: true });
 export default mongoose.model('Subscription', SubscriptionSchema);
+
+
